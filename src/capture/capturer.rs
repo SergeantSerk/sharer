727a115f65fe7e121e7413e960f2a02f9909fec3
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2025-03-15 17:00:36 +0000

    Revert "Disable audio capture"
    This reverts commit 637fa3eef246e88e1b7c4f22ed49e49aa2eb7790.

--- src/capture/capturer.rs
+++ src/capture/capturer.rs
@@ -228,7 +228,7 @@ impl Capturer {
                 };
 
                 #[cfg(target_os = "windows")]
-                //AudioCapture::capture(output.clone(), shutdown_token.clone()).unwrap();
+                AudioCapture::capture(output.clone(), shutdown_token.clone()).unwrap();
 
                 capture
                     .start_capture(encoder, output, profiler, shutdown_token.clone())
